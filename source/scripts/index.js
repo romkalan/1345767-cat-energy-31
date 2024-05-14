f085ac398e09f72cf82dc776c3a704f07c6fe2fc
romkalan <romkalan@mail.ru>
2024-05-14 21:59:59 +0300

    add script with open and close menu navigation for mobile version

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+let mainHeader = document.querySelector('.main-header');
+let mainNavList = document.querySelector('.main-navigation__list');
+let mainHeaderToggle = document.querySelector('.main-header__toggle');
+
+mainHeaderToggle.addEventListener('click', function () {
+  if (mainNavList.classList.contains('site-list--opened')) {
+    mainNavList.classList.remove('site-list--opened');
+    mainNavList.classList.add('site-list');
+    mainHeader.classList.remove('main-header--opened');
+  } else {
+    mainNavList.classList.remove('site-list');
+    mainNavList.classList.add('site-list--opened');
+    mainHeader.classList.add('main-header--opened');
+  }
+});
